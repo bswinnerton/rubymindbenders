5169f16d2795d24778aaa0c440039a0fecc8dd0a
Brooks Swinnerton <bswinnerton@gmail.com>
2014-01-04 19:22:40 -0500

    Adding track 2 answer

--- 02 - FizzBuzz/lib/fizz_buzz.rb
+++ 02 - FizzBuzz/lib/fizz_buzz.rb
@@ -1,5 +1,10 @@
 class FizzBuzz
   def generally_fizzy(divisor_array, low, high)
-    # View README.md for instructions
+    low.upto(high) do |n|
+      divisible = divisor_array.inject([]) { |a,e| a << (n % e == 0) }
+      puts "#{n} reallyfizzy" if divisible.all? { |e| e == true }
+      puts "#{n} fizzy" if divisible.include? true and divisible.include? false
+      puts n if divisible.all? { |e| e == false }
+    end 
   end
 end
