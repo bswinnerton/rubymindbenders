f6c606676b0b0ed108f3935ba1c8e55510c02ede
snebel <sam@samnebel.com>
2014-01-07 17:34:19 -0500

    refactored prime? method

--- 03 - Prime/lib/fixnum.rb
+++ 03 - Prime/lib/fixnum.rb
@@ -1,7 +1,6 @@
 class Fixnum
   def prime?
-  	return false if self == 1
-    2.upto(Math.sqrt(self).to_i) { |n| return false if self % n == 0 }
-    return true
+		2.upto(Math.sqrt(self).to_i) { |n| return false if self % n == 0 }
+		self == 1 ? false : true
   end
 end
