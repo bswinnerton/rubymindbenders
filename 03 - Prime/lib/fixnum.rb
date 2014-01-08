c4c22d5fc3a6de5c65127df0ca26bfa90c89a8f1
Brooks Swinnerton <bswinnerton@gmail.com>
2014-01-08 09:26:25 -0500

    Update fixnum.rb

--- 03 - Prime/lib/fixnum.rb
+++ 03 - Prime/lib/fixnum.rb
@@ -1,6 +1,6 @@
 class Fixnum
   def prime?
-		2.upto(Math.sqrt(self).to_i) { |n| return false if self % n == 0 }
-		self == 1 ? false : true
+    2.upto(Math.sqrt(self).to_i) { |n| return false if self % n == 0 }
+    self == 1 ? false : true
   end
 end
