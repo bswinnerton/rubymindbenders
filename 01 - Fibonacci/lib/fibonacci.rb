5cec7a117409f0c83aab01ec2ec5015231b545e1
Brooks Swinnerton <bswinnerton@gmail.com>
2014-01-10 17:54:42 -0500

    Tidying fibonacci answer

--- 01 - Fibonacci/lib/fibonacci.rb
+++ 01 - Fibonacci/lib/fibonacci.rb
@@ -1,5 +1,4 @@
 class Fibonacci
-  # tail recursive
   def self.calculate(n, previous_sum = 0, current_sum = 1)
     n < 2 ? current_sum : calculate(n - 1, current_sum, current_sum + previous_sum)
   end
