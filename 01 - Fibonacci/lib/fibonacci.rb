9b3c6615e9a8710f77930390b0a3b01f4e2b6a5c
snebel <sam@samnebel.com>
2014-01-10 09:23:08 -0500

    tail recursive Fibonacci.calculate

--- 01 - Fibonacci/lib/fibonacci.rb
+++ 01 - Fibonacci/lib/fibonacci.rb
@@ -1,6 +1,25 @@
 class Fibonacci
-  def self.calculate(n)
-    return n if [0,1].include? n
-    calculate(n - 1) + calculate(n - 2)
+
+  # standard recursive solution - wildly inefficient,
+  # obscenely slow for n above 40
+  #
+  # def self.calculate(n)
+  #   return n if [0,1].include? n
+  #   calculate(n - 1) + calculate(n - 2)
+  # end
+
+  # tail-recursive solution - way faster, 
+  # no repeated computations
+  #
+  def self.calc_helper(n, prev, sum)
+  	n < 2 ? sum : calc_helper(n-1, sum, sum + prev)
   end
+
+  def self.calculate(n)
+  	calc_helper(n, 0, 1)
+  end
+end
+
+1.upto(15) do |n|
+	puts Fibonacci.calculate(n)
 end
