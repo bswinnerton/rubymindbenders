f8bbd13b41f6e83a1a241492b7ea0a14cb79702f
Brooks Swinnerton <bswinnerton@gmail.com>
2014-01-11 18:51:18 -0500

    Adding track 4

---
+++ 04 - Spiral/lib/array.rb
@@ -0,0 +1,7 @@
+class Array
+  def spiral
+    # View README.md for instructions
+    # Hint:
+    #  - Remember that since you'll be calling matrix.spiral, matrix would be self inside this method
+  end
+end
