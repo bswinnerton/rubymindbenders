05575679d15174c23605557e2fc65595f6e34501
Brooks Swinnerton <bswinnerton@gmail.com>
2014-01-13 23:52:30 -0500

    Refining track 4 answer

--- 04 - Spiral/lib/array.rb
+++ 04 - Spiral/lib/array.rb
@@ -1,8 +1,13 @@
 class Array
-  def spiral
-    array = []
-    array << self.shift
-    array << self.transpose.reverse.spiral unless self.empty?
-    array.flatten
+  def spiral(spin = 0)
+    matrix = []
+    case spin
+    when 0 then matrix << self.shift
+    when 1 then matrix << self.map(&:pop)
+    when 2 then matrix << self.pop.reverse
+    when 3 then matrix << self.reverse.map(&:shift)
+    end
+    matrix << spiral((spin + 1) % 4) if self.any?
+    matrix.flatten
   end
 end
